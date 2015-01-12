8c66dfcb744eedf539287976b9e2ceb0880bfd37
mikegtr <gatner@gmail.com>
2015-01-12 15:52:47 +0100

    file upload

--- server.js
+++ server.js
@@ -5,7 +5,9 @@ var express        = require('express');
 var app            = express();
 var bodyParser     = require('body-parser');
 var methodOverride = require('method-override');
-
+var fs     		   = require('fs');
+var busboy         = require('connect-busboy');
+var path 		   = require('path');
 // configuration ===========================================
     
 // config files
@@ -21,6 +23,7 @@ var port = process.env.PORT || 8080;
 // get all data/stuff of the body (POST) parameters
 // parse application/json 
 app.use(bodyParser.json()); 
+app.use(busboy());
 
 // parse application/vnd.api+json as json
 app.use(bodyParser.json({ type: 'application/vnd.api+json' })); 
@@ -28,12 +31,28 @@ app.use(bodyParser.json({ type: 'application/vnd.api+json' }));
 // parse application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({ extended: true })); 
 
+
 // override with the X-HTTP-Method-Override header in the request. simulate DELETE/PUT
 app.use(methodOverride('X-HTTP-Method-Override')); 
 
 // set the static files location /public/img will be /img for users
 app.use(express.static(__dirname + '/')); 
 
+
+
+app.post('/upload', function (req, res) {
+    req.pipe(req.busboy);
+    req.busboy.on('file', function (fieldname, file, filename) {
+      var stream = fs.createWriteStream(__dirname + '/upload/' + filename);
+      file.pipe(stream);
+      stream.on('close', function () {
+        console.log('File ' + filename + ' is uploaded');
+        res.json({
+          filename: filename
+        });
+      });
+    });
+  });
 // routes ==================================================
 //require('./app/routes')(app); // configure our routes
    /*app.get('*', function(req, res) {
